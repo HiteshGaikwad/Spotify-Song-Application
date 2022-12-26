c229956acfdac29366291b09f70587e14cd5a6df
Hitesh <gaikwadhitesh03@gmail.com>
2022-12-26 18:27:47 +0530

    first

---
+++ Downloads/sms/sms/src/main/java/com/codewalla/sms/Student.java
@@ -0,0 +1,19 @@
+package com.codewalla.sms;
+
+public class Student {
+
+    public String name;
+
+    public int id;
+
+    public String course;
+
+    public int age;
+
+    public Student(String name, int id, String course, int age) {
+        this.name = name;
+        this.id = id;
+        this.course = course;
+        this.age = age;
+    }
+}
